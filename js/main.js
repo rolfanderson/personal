ee3d779b1c1a602b7144c957d5451bd85d9803d4
Rolf Anderson <rolf.k.anderson@gmail.com>
2017-09-01 13:33:30 -0400

    update

--- js/main.js
+++ js/main.js
@@ -19,3 +19,11 @@ jQuery(document).ready(function($) {
         window.location = $(this).data("href");
     });
 });
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 80) {
+    $('nav').addClass('shrink');
+  } else {
+    $('nav').removeClass('shrink');
+  }
+});
--- js/map.js
+++ js/map.js
@@ -6,10 +6,47 @@ var personal = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 });
 
 map = L.map("map", {
-  zoom: 4,
-  center: [42.877742, -97.380979],
+  zoom: 5,
+  center: [38, -95],
   layers: [personal],
   zoomControl: false,
   scrollWheelZoom: true,
   attributionControl: false
 });
+
+var npsIcon = L.icon({
+              iconUrl: 'img/button-nps.png',
+              iconSize: [35, 35],
+});
+
+var fcIcon = L.icon({
+              iconUrl: 'img/button-fc.png',
+              iconSize: [35, 35],
+});
+
+var flIcon = L.icon({
+              iconUrl: 'img/button-fl.png',
+              iconSize: [25, 25],
+});
+
+var pmIcon = L.icon({
+              iconUrl: 'img/button-pm.png',
+              iconSize: [25, 25],
+});
+
+var phIcon = L.icon({
+              iconUrl: 'img/button-ph.png',
+              iconSize: [25, 25],
+});
+
+var marker =
+
+  L.marker([44.6621, -111.1041], {icon: npsIcon}).addTo(map).bindPopup("<a class='popup'>Post Card</a>");
+
+  L.marker([36.998976, -109.045172], {icon: fcIcon}).addTo(map).bindPopup("<a class='popup'>Four Corners</a>");
+
+  //L.marker([28.5, -82], {icon: flIcon}).addTo(map).bindPopup("<a class='popup'>State Park</a>");
+
+  L.marker([28.4187304,-81.581206], {icon: pmIcon}).addTo(map).bindPopup("<a class='popup'>Death Tour</a>");
+
+  L.marker([29.67264339999999,-82.33887549999997], {icon: phIcon}).addTo(map).bindPopup("<a class='popup'>UTBAPH</a>");
